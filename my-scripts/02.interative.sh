012bef798af227379ae4143bcfb874b9a59b1287
Angus <me@angusgoldman.com.au>
2023-05-07 16:51:30 +1000

    Fix relative dir from moving

--- my-scripts/02.interative.sh
+++ my-scripts/02.interative.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-winpty docker run --name="ETXDocker" -it --rm --mount src="$(pwd)",target="/src",type=bind ghcr.io/edgetx/edgetx-dev:2.8 bash -c 'cd /src; bash'
+winpty docker run --name="ETXDocker" -it --rm --mount src="$(pwd)/..",target="/src",type=bind ghcr.io/edgetx/edgetx-dev:2.8 bash -c 'cd /src; bash'
